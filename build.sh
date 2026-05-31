7649b07938615a15a938a2731a31af4b48bf7fd1
Finnian Brewer <finnian.m.brewer@gmail.com>
2026-05-31 14:51:50 -0700

    remove erlang installation

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 
 GLEAM_VERSION="1.16.0"
 
-apt-get install -y erlang
-
 mkdir -p bin
 
 curl -fsSL "https://github.com/gleam-lang/gleam/releases/download/v${GLEAM_VERSION}/gleam-v${GLEAM_VERSION}-x86_64-unknown-linux-musl.tar.gz" \
